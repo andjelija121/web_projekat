ba33466eeb678a08146c44d16e5cbb14c5cd77bf
Magdalena Lazarevic <magdalenalazarevic30@gmail.com>
2026-04-23 19:05:20 +0200

    napravljen fajl katalog knjiga, pojedinacna knjiga i admin knjiga i dodate u sajd bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes,Route, Navigate } from "react-router-dom"
 import AppKostur from "./layout/AppKostur"
 import Autori from "./stranice/Autori"
 import AutoriAdmin from "./stranice/AutoriAdmin"
+import KatalogKnjiga from "./stranice/KatalogKnjiga"
+import KnjigaAdmin from "./stranice/KnjigaAdmin"
+import PojedinacnaKnjiga from "./stranice/PojedinacnaKnjiga"
 
 function App() {
 
@@ -12,6 +15,10 @@ function App() {
               <Route index element={<Navigate to="/autori" replace />}/>
               <Route path='/autori' element={<Autori/>}/>
               <Route path='/autoriAdmin' element={<AutoriAdmin/>}/>
+              <Route index element={<Navigate to="/knjige" replace />}/>
+              <Route path='/knjige' element={<KatalogKnjiga/>}/>
+              <Route path='/knjigaAdmin' element={<KnjigaAdmin/>}/>
+              <Route path='/pojedinacnaKnjiga' element={<PojedinacnaKnjiga/>}/>
           </Route>
         </Routes>
     </BrowserRouter>
---
+++ src/stranice/PojedinacnaKnjiga.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PojedinacnaKnjiga() {
+  return (
+    <div>PojedinacnaKnjiga</div>
+  )
+}
+
+export default PojedinacnaKnjiga
