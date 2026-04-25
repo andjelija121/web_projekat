d90075ee8aea5643d5af16675d9ab61253590f00
Magdalena Lazarevic <magdalenalazarevic30@gmail.com>
2026-04-25 22:18:39 +0200

    pravljenje katrice za katalog knjiga i dodavanje u stranicu katalog knjiga

--- src/komponente/katicaKnjige.jsx
+++ src/komponente/katicaKnjige.jsx
@@ -2,37 +2,29 @@
 
 import React from "react";
 
-const karticaKnjige = ({ slika, naslov, autor, ocena, cena }) => {
+const KarticaKnjige = ({ slika, naziv, imeAutora, prezimeAutora, ocena, cena }) => {
   return (
-    <div className="w-[320px] bg-[#d8d0ff] rounded-2xl p-4 shadow-md flex gap-4 items-center hover:scale-105 transition duration-300">
+    <div className="w-[320px] bg-bela rounded-2xl p-4 shadow-md flex gap-4 items-center hover:scale-105 transition duration-300">
       
-      {/* Slika knjige */}
+    
       <img
         src={slika}
-        alt={naslov}
+        alt={naziv}
         className="w-[90px] h-[130px] object-cover rounded-lg shadow"
       />
 
-      {/* Desni deo kartice */}
       <div className="flex flex-col justify-between h-full">
         
-        {/* Naslov i autor */}
         <div>
-          <h2 className="text-lg font-bold text-white leading-tight">
-            {naslov}
+          <h2 className="text-lg font-bold text-brown leading-tight">
+            {naziv}
           </h2>
-          <p className="text-sm text-gray-200 mt-1">
-            by {autor}
+          <p className="text-sm text-brown-200 mt-1">
+            by {imeAutora} <span>{prezimeAutora}</span>
           </p>
         </div>
 
-        {/* Zvezdice */}
-        <div className="flex text-yellow-400 text-lg mt-2">
-          ★★★★★
-        </div>
-
-        {/* Cena */}
-        <p className="text-white font-semibold text-md mt-3">
+        <p className="text-brown font-semibold text-md mt-3">
           {cena} RSD
         </p>
       </div>
@@ -40,4 +32,4 @@ const karticaKnjige = ({ slika, naslov, autor, ocena, cena }) => {
   );
 };
 
-export default karticaKnjige;
+export default KarticaKnjige;
--- src/stranice/KatalogKnjiga.jsx
+++ src/stranice/KatalogKnjiga.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import karticaKnjige from "../komponente/katicaKnjige";
-import data from "../data/knjige.json";
+import KarticaKnjige from "../komponente/katicaKnjige";
+import data from "../data.json"
 
 
 const KatalogKnjiga = () => {
-  const knjige = Object.values(data);
-  console.log(knjige)
+  const knjige = Object.values(data.knjige);
+  const autori = Object.values(data.autori);
 
   return (
     <div className="min-h-screen bg-[#f5f3ff] p-10">
-      <h1 className="text-4xl font-bold text-center mb-10">
+      <h1 className="text-4xl font-bold text-left ml-16 mb-10">
         Katalog knjiga
-      </h1>
+      </h1> 
 
       <div className="flex flex-wrap justify-center gap-8">
         {knjige.map((knjiga,index) => (
           <KarticaKnjige
             key={index}
             slika={knjiga.slike[0]}
-            naslov={knjiga.naslov}
-            autor={knjiga.autor}
+            naziv={knjiga.naziv}
+            imeAutora={autori[parseInt(knjiga.id_autora.slice(-1))-1].ime}
+            prezimeAutora={autori[parseInt(knjiga.id_autora.slice(-1))-1].prezime}
             ocena={knjiga.ocena}
             cena={knjiga.cena}
           />
