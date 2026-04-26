ee347af0e88a77154e7c59e9008b996d6a229314
unknown <andjelijamitic06@gmail.com>
2026-04-26 03:06:23 +0200

    pretraga po imenu i po statusu

--- src/stranice/Autori.jsx
+++ src/stranice/Autori.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import AutorKartica from '../komponente/AutorKartica'
 import { NavLink } from 'react-router-dom'
 import data from '../data.json'
+import PretragaAutori from '../komponente/PretragaAutori'
 
 
 function Autori() {
   const autori = Object.values(data.autori)
   return (
-    <div className='grid grid-cols-3 justify-items-center items-cente gap-4'>{autori.map((autor,index)=>(
-      <NavLink key={index} to={`/autori/${index}`}>
+    <div className='p-10'>
+      <div className='flex justify-between'>
+        <h1 className='font-bold text-5xl mb-20'>
+          Истражи ауторе
+        </h1>
+        <PretragaAutori/>
+      </div>
+      <div className='grid grid-cols-3 justify-items-center items-cente gap-2 gap-y-20'>{autori.map((autor,index)=>(
+        <NavLink key={index} to={`/autori/${index}`}>
 
-        <AutorKartica autor={autor}/>
-      </NavLink>
-    ))}
+          <AutorKartica autor={autor}/>
+        </NavLink>
+      ))}
+      </div>
     </div>
   )
 }
