98ce820a61470daa75c53982eb06bcb54cfe7d09
unknown <andjelijamitic06@gmail.com>
2026-04-27 20:01:21 +0200

    dizajn autori detalji stranice

--- src/komponente/AutorOpis.jsx
+++ src/komponente/AutorOpis.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import data from '../data.json'
+import { NavLink } from 'react-router-dom';
 
 function AutorOpis({autor,autorId}) {
     let [opcija,setOpcija] = useState("biografija")
 
     let nizKnjiga = Object.values(data.knjige);
-    let knjige = nizKnjiga.filter((el)=> el.id_autora===`autor${parseInt(autorId)+1}`)
+    let knjige = nizKnjiga.map((el,i)=>({ ...el, id: i })).filter((el)=> el.id_autora===`autor${parseInt(autorId)+1}`)
     let sekcija = {
-        "biografija":<Biografija tekst={autor.biografija}/>,
+        "biografija":<Biografija tekst={autor.biografija} slike={autor.slike}/>,
         "karijera":<Karijera autor={autor}/>,
-        "slike":<Slike slike={autor.slike}/>,
         "knjige":<Knjige knjige={knjige}/>,
 
     }
@@ -19,14 +19,14 @@ function AutorOpis({autor,autorId}) {
     }
 
     return (
-    <div className='grid grid-cols-[30%_70%] h-full overflow-hidden'>
-         <div className='flex flex-col gap-10'>
-      {["biografija","karijera","slike","knjige"].map((opt) => (
+    <div className='grid grid-cols-[20%_80%] h-full overflow-hidden'>
+         <div className='flex flex-col gap-10 border-r-2 border-dark-coffee-400 h-[97%]'>
+      {["biografija","karijera","knjige"].map((opt) => (
         <button
           key={opt}
           onClick={() => handleChange(opt)}
           className={`
-            border-l-4 text-base px-5 py-3 text-left transition-colors
+            border-l-4 text-lg px-5 py-3 text-left transition-colors cursor-pointer
             ${opcija === opt 
               ? 'border-l-dark-coffee-900 font-semibold' 
               : 'border-l-dark-coffee-400 '
@@ -38,7 +38,7 @@ function AutorOpis({autor,autorId}) {
       ))}
     </div>
 
-        <div className='p-5 overflow-y-auto h-full'>
+        <div className=' overflow-y-auto h-full pb-10 px-5 '>
             {sekcija[opcija]}
         </div>
     </div>
@@ -46,36 +46,40 @@ function AutorOpis({autor,autorId}) {
 }
 
 
-function Biografija({tekst}){
-    return <p>
-        {tekst}
-    </p>
+function Biografija({tekst,slike}){
+    return <div className='grid grid-cols-[1fr_400px] gap-18'>
+
+         <p className='text-lg leading-10 text-dark-coffee-700'>
+            {tekst}
+        </p>
+        <img src={slike[0]} className='aspect-3/4 rounded-xl object-cover '/>
+    </div>
 }
 
 
 function Karijera({autor}){
-    return <div className='flex flex-col items-strech justify-around h-full'>
-        <div className='flex items-center justify-between border-b-2 border-dark-coffee-400 px-2 py-8 '>
-            <p className=' text-3xl'>
+    return <div className='flex flex-col '>
+        <div className='flex items-center justify-between border-b-2 border-dark-coffee-400 px-2 pb-8 '>
+            <p className=' text-3xl text-dark-coffee-700'>
                 Датум рођења
             </p>
             <span className='font-bold text-xl'>{autor.datum_rodjenja}</span>
         </div>
         <div className='flex items-center justify-between border-b-2 border-dark-coffee-400  px-2 py-8'>
-            <p className=' text-3xl'>
+            <p className=' text-3xl text-dark-coffee-700'>
                 Број освојених награда током каријере:
             </p>
             <span className='font-bold text-xl'>{autor.broj_nagrada}</span>
         </div>
         <div className='flex items-center justify-between border-b-2 border-dark-coffee-400 px-2 py-8'>
-            <p className=' text-3xl'>
+            <p className=' text-3xl text-dark-coffee-700'>
                 Укупан број продатих примерака свих књига
             </p>
             <span className='font-bold text-xl'>{autor.broj_prodatih_primeraka}</span>
         </div>
         <div className='flex items-center justify-between border-b-2  border-dark-coffee-400 px-2 py-8'>
-            <p className=' text-3xl'>
-                Котакт телефон менаџera
+            <p className=' text-3xl text-dark-coffee-700'>
+                Котакт телефон менаџeра
             </p>
             <span className='font-bold text-xl'>{autor.kontakt_telefon_menadzera}</span>
         </div>
@@ -93,12 +97,18 @@ function Slike({slike}){
 
 function Knjige({ knjige }) {
   return (
-    <div>
+    <div className='grid grid-cols-2 gap-3'>
       {knjige.map((k, i) => (
-        <p key={i}>{k.naziv}</p>
+        <NavLink key={i} to={`/knjige/${k.id}`}>
+
+        <div key={i} className='bg-bela border border-dark-coffee-400 rounded-xl p-4'>
+          <p className='font-semibold text-braon'>{k.naziv}</p>
+          <p className='text-sm text-dark-coffee-500 mt-1'>{k.godina}</p>
+        </div>
+        </NavLink>
       ))}
     </div>
   );
 }
 
-export default AutorOpis
+export default AutorOpis;
